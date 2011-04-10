cd0fc99b7b4452f1f322e0a0c986bb1ef0c2b91f
Fredrik Blomqvist <fredrik.blomqvist.95@gmail.com>
2011-04-10 20:34:13 +0200

    Incremented version to Beta 3

--- MKV Chapterizer/MKVC.cs
+++ MKV Chapterizer/MKVC.cs
@@ -432,7 +432,7 @@ namespace MKV_Chapterizer
             }
             lblTrackbarValue.Text = trackBar1.Value.ToString();
 
-            label9.Text = "v" + Convert.ToString(GetVersion(Version.Parse(Application.ProductVersion))) + " Beta 2";
+            label9.Text = "v" + Convert.ToString(GetVersion(Version.Parse(Application.ProductVersion))) + " Beta 3";
             
 
         }
