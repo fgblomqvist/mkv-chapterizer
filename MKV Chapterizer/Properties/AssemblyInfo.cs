01efde625a1e7488c9cac48678d38d346246a103
Fredrik Blomqvist <fredrik.blomqvist.95@gmail.com>
2011-12-04 11:30:48 +0100

    Incremented version to 2.1.1

--- MKV Chapterizer/Properties/AssemblyInfo.cs
+++ MKV Chapterizer/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("MKV Chapterizer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Cyb3rH4Xter")]
+[assembly: AssemblyCompany("Fredrik Blomqvist")]
 [assembly: AssemblyProduct("MKV Chapterizer")]
 [assembly: AssemblyCopyright("Copyright ©  2011")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.1.0.0")]
-[assembly: AssemblyFileVersion("2.1.0.0")]
+[assembly: AssemblyVersion("2.1.1.0")]
+[assembly: AssemblyFileVersion("2.1.1.0")]
